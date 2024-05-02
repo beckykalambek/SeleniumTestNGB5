8553a1f278ba9b51173edb0b555278af62ad75f3
beckykalambek <beckykalambek@gmail.com>
2024-05-02 07:56:55 -0500

    conflict utomated

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -1,14 +1,16 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class ConflictTests {
-
+    @Test
     public void test1 (){
         int a = 5;
         int b = 20;
         System.out.println("sum: " + (a+b));
     }
+    @Test
     public void test2 (){
         String message = "Welcome to USA";
         Assert.assertTrue(message.contains("USA"));
