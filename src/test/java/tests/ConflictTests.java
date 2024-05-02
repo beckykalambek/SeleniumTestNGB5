2dab53119820bb6a8eb5ce0b886f0fa2e34d7089
beckykalambek <beckykalambek@gmail.com>
2024-05-02 08:09:35 -0500

    modified test2 method

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -12,7 +12,7 @@ public class ConflictTests {
     }
     @Test
     public void test2 (){
-        String message = "Welcome to USA";
+        String message = "Welcome to USA - the land of opportunities";
         Assert.assertTrue(message.toLowerCase().contains("usa"));
     }
 }
