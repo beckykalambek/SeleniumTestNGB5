e1ab13b43bfe2fec68bb98e598cf913d4531024e
beckykalambek <167790796+beckykalambek@users.noreply.github.com>
2024-05-02 09:10:51 -0500

    Update ConflictTests.java

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -9,11 +9,12 @@ public class ConflictTests {
         int a = 5;
         int b = 20;
         int sum = (a+b) * 2;
+        
         System.out.println("sum: " + sum);
     }
     @Test
     public void test2 (){
         String message = "Welcome to USA - the land of opportunities";
-        Assert.assertTrue(message.toLowerCase().contains("usa"));
+        Assert.assertTrue(message.toLowerCase().contains("land"));
     }
 }
